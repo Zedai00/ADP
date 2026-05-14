f0a9f8295b89369c33758039b0122c67a28f0597
Jishan Alam <jishanalam9128@gmail.com>
2026-05-14 22:33:34 +0530

    Executable Perms

--- customize.sh
+++ customize.sh
@@ -1,3 +1,6 @@
+ui_print "- Giving Executable Permission"
+chmod +x /data/adb/modules/adp/bin/adp
+
 ui_print "- Symlinking adp to KSU bin"
 
 mkdir -p /data/adb/ksu/bin
@@ -5,3 +8,4 @@ mkdir -p /data/adb/ksu/bin
 ln -sf \
     /data/adb/modules/adp/bin/adp \
     /data/adb/ksu/bin/adp
+
